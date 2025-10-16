1980041b32b309834e04442ce7716ebb31352674
LorenzoOliveira-git <lorenzolimadeoliveira2010@gmail.com>
2025-10-16 20:58:30 -0300

    primeira versão da aplicação

---
+++ src/main/java/com/agenda/MODEL/JavaBeans.java
@@ -0,0 +1,5 @@
+package com.agenda.MODEL;
+
+public class JavaBeans {
+
+}
---
+++ src/main/java/com/agenda/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.agenda.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/controller", "/main"})
+public class Controller extends HttpServlet {
+
+}
